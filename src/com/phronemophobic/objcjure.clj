2331c4a2f9d96b78e29317363d21c7229d1686fa
Adrian Smith <smith.adriane@gmail.com>
2024-10-01 20:01:50 -0700

    Fix using structs as args.

--- src/com/phronemophobic/objcjure.clj
+++ src/com/phronemophobic/objcjure.clj
@@ -118,6 +118,8 @@
     (instance? Double arg) :float64
     (instance? Float arg) :float32
     (char? arg) :int8
+    (instance? tech.v3.datatype.struct.Struct) (dtype-proto/datatype arg)
+
     (dt-ffi/convertible-to-pointer? arg) :pointer
 
     :else (throw (ex-info "Unsupported arg dtype"
